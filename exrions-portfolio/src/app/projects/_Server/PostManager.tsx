109afe271b43f8bbd01eae397d4fd9f78b5c5dfe
Cai Xuan <exrion.lexom@gmail.com>
2026-05-04 20:50:11 +0800

    removed try catch debug code

--- exrions-portfolio/src/app/projects/_Server/PostManager.tsx
+++ exrions-portfolio/src/app/projects/_Server/PostManager.tsx
@@ -6,26 +6,20 @@ import { STR_PROJECTS_DIRECTORY } from '../../_Utilities/constants';
 import * as fs from "fs";
 
 export async function getPostData(id: string): Promise<string> {
-    try {
-        const fullPath = path.join(process.cwd(), STR_PROJECTS_DIRECTORY, `${id}.md`.replaceAll("%20", " "));
-        const fileContents = fs.readFileSync(fullPath, 'utf8');
+    const fullPath = path.join(process.cwd(), STR_PROJECTS_DIRECTORY, `${id}.md`.replaceAll("%20", " "));
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
 
-        // Use gray-matter to parse the post metadata section
-        const matterResult = matter(fileContents);
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
 
-        // Use remark to convert markdown into HTML string
-        // const processedContent = await remark()
-        //     .use(html)
-        //     .process(matterResult.content);
-        // const contentHtml = processedContent.toString();
+    // Use remark to convert markdown into HTML string
+    // const processedContent = await remark()
+    //     .use(html)
+    //     .process(matterResult.content);
+    // const contentHtml = processedContent.toString();
 
-        // Combine the data with the id and contentHtml
-        return JSON.stringify(matterResult);
-    }
-    catch (e: any) {
-        console.log(e);
-        return JSON.stringify({ error: e.message });
-    }
+    // Combine the data with the id and contentHtml
+    return JSON.stringify(matterResult);
 }
 
 export async function getAllPostIds(): Promise<string[]> {
